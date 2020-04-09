d46a1e35ddee47c882c5a3f86fd48c0075bc16e0
Ethan Firpo <efirpo@gmail.com>
2020-04-08 20:42:29 -0700

    add checkForWin prototype

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,18 @@ function Game() {
 }
 
 Game.prototype.addBox = function () {
-  for (i = 0; i <= this.player1.length; i++) {
+  for (i = 0; i < this.player1.length; i++) {
     this.pOneBoxes.splice(this.player1[i], 1, 1)
   }
-  for (i = 0; i <= this.player2.length; i++) {
+  for (i = 0; i < this.player2.length; i++) {
     this.pTwoBoxes.splice(this.player2[i], 1, 1)
   }
 }
 
 Game.prototype.checkForWin = function () {
-
+  if (this.pOneBoxes[0] + this.pOneBoxes[1] + this.pOneBoxes[2] === 3) {
+    $(".winner").append("It's a WIN for X!")
+  }
 }
 
 // function winConditions() {
@@ -36,36 +38,36 @@ Game.prototype.checkForWin = function () {
 //   if ((game.player1).includes("0") && (game.player1).includes("1") && (game.player1).includes("2")) {
 //     $("#winner").append("X player wins!")
 //   }
-  // else if ((game.player1).includes(win2[i]) && (game.player1).includes(win2[i + 1]) && (game.player1).includes(win2[i + 2])) {
-  //   $("#winner").append("X player wins!")
-  // }
-  // else if ((game.player1).includes(win3[i]) && (game.player1).includes(win3[i + 1])(game.player1).includes(win3[i + 2])) {
-  //   $("#winner").append("X player wins!")
-  // }
-  // else if ((game.player1).includes(win4[i]) && (game.player1).includes(win4[i + 1]) && (game.player1).includes(win4[i])) {
-  //   $("#winner").append("X player wins!")
-  // }
-  // else if ((game.player1).includes(win5[i]) && (game.player1).includes(win5[i + 1]) && (game.player1).includes(win5[i])) {
-  //   $("#winner").append("X player wins!")
-  // }
-  // else if ((game.player1).includes(win6[i]) && (game.player1).includes(win6[i + 1]) && (game.player1).includes(win6[i])) {
-  //   $("#winner").append("X player wins!")
-  // }
-  // else if ((game.player1).includes(win7[i]) && (game.player1).includes(win7[i + 1]) && (game.player1).includes(win7[i])) {
-  //   $("#winner").append("X player wins!")
-  // }
-  // else if ((game.player1).includes(win8[i]) && (game.player1).includes(win8[i + 1]) && (game.player1).includes(win8[i])) {
-  //   $("#winner").append("X player wins!")
-  // }
-}
+// else if ((game.player1).includes(win2[i]) && (game.player1).includes(win2[i + 1]) && (game.player1).includes(win2[i + 2])) {
+//   $("#winner").append("X player wins!")
+// }
+// else if ((game.player1).includes(win3[i]) && (game.player1).includes(win3[i + 1])(game.player1).includes(win3[i + 2])) {
+//   $("#winner").append("X player wins!")
+// }
+// else if ((game.player1).includes(win4[i]) && (game.player1).includes(win4[i + 1]) && (game.player1).includes(win4[i])) {
+//   $("#winner").append("X player wins!")
+// }
+// else if ((game.player1).includes(win5[i]) && (game.player1).includes(win5[i + 1]) && (game.player1).includes(win5[i])) {
+//   $("#winner").append("X player wins!")
+// }
+// else if ((game.player1).includes(win6[i]) && (game.player1).includes(win6[i + 1]) && (game.player1).includes(win6[i])) {
+//   $("#winner").append("X player wins!")
+// }
+// else if ((game.player1).includes(win7[i]) && (game.player1).includes(win7[i + 1]) && (game.player1).includes(win7[i])) {
+//   $("#winner").append("X player wins!")
+// }
+// else if ((game.player1).includes(win8[i]) && (game.player1).includes(win8[i + 1]) && (game.player1).includes(win8[i])) {
+//   $("#winner").append("X player wins!")
+// }
+//}
 
 
 // UI Logic   
 
-var game = new Game();
-$(document).ready(function () {
 
-  var wins = new winConditions();
+$(document).ready(function () {
+  var game = new Game();
+  // var wins = new winConditions();
   $("#startGame").click(function () {
     $(".game").show();
   })
@@ -79,8 +81,10 @@ $(document).ready(function () {
         game.turn += 1;
         game.player2.push(this.id);
         //      game.player2.sort();
-        wins.checkForWinner();
-
+        // wins.checkForWinner();
+        console.log(game.pOneBoxes);
+        game.addBox();
+        game.checkForWin();
       }
       else if ((game.turn % 2) !== 0) {
         $(this).html("X");
@@ -89,7 +93,7 @@ $(document).ready(function () {
         game.player1.push(this.id);
         //console.log(game.player1);
         //      game.player1.sort();
-        wins.checkForWinner();
+        //    wins.checkForWinner();
       }
     }
 
