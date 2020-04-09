76f1bb4d9283bace6e177c8ba337180e1bfb8a65
Ethan Firpo <efirpo@gmail.com>
2020-04-09 13:22:58 -0700

    add styles for  index

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,21 @@ function Game() {
   this.turn = 1; // <---keeps track of whose turn it is
   this.pOneBoxes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   this.pTwoBoxes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  this.Winner = "";
+}
+
+function Players() {
   this.pOneWins = 0;
   this.pTwoWins = 0;
+  this.pOneName = "";
+  this.pTwoName = "";
+
 }
 
 Game.prototype.addBox = function () {
   for (i = 0; i < this.player1.length; i++) {
     this.pOneBoxes.splice(this.player1[i], 1, 1)
+    // this.pOneBoxes[this.player1[i]] = 1;
   }
   for (i = 0; i < this.player2.length; i++) {
     this.pTwoBoxes.splice(this.player2[i], 1, 1)
@@ -20,19 +28,21 @@ Game.prototype.addBox = function () {
 }
 
 Game.prototype.checkForWin = function () {
+  console.log(this.pTwoBoxes);
   if (this.pOneBoxes[0] + this.pOneBoxes[1] + this.pOneBoxes[2] === 3 || this.pOneBoxes[3] + this.pOneBoxes[4] + this.pOneBoxes[5] === 3 || this.pOneBoxes[6] + this.pOneBoxes[7] + this.pOneBoxes[8] === 3 || this.pOneBoxes[0] + this.pOneBoxes[3] + this.pOneBoxes[6] === 3 || this.pOneBoxes[1] + this.pOneBoxes[4] + this.pOneBoxes[7] === 3 || this.pOneBoxes[2] + this.pOneBoxes[5] + this.pOneBoxes[8] === 3 || this.pOneBoxes[0] + this.pOneBoxes[4] + this.pOneBoxes[8] === 3 || this.pOneBoxes[2] + this.pOneBoxes[4] + this.pOneBoxes[6] === 3) {
     $("#winner").append("It's a WIN for X!");
-    this.pOneWins += 1;
-    $(".box").empty();
+    this.Winner = "yes";
   }
   else if (this.pTwoBoxes[0] + this.pTwoBoxes[1] + this.pTwoBoxes[2] === 3 || this.pTwoBoxes[3] + this.pTwoBoxes[4] + this.pTwoBoxes[5] === 3 || this.pTwoBoxes[6] + this.pTwoBoxes[7] + this.pTwoBoxes[8] === 3 || this.pTwoBoxes[0] + this.pTwoBoxes[3] + this.pTwoBoxes[6] === 3 || this.pTwoBoxes[1] + this.pTwoBoxes[4] + this.pTwoBoxes[7] === 3 || this.pTwoBoxes[2] + this.pTwoBoxes[5] + this.pTwoBoxes[8] === 3 || this.pTwoBoxes[0] + this.pTwoBoxes[4] + this.pTwoBoxes[8] === 3 || this.pTwoBoxes[2] + this.pTwoBoxes[4] + this.pTwoBoxes[6] === 3) {
     $("#winner").append("It's a WIN for O!");
-    this.pTwoWins += 1;
-    $(".box").empty();
+    this.Winner = "yes";
+    //   console.log('p2 wins');
   }
 
-  else if (this.pOneBoxes[0] + this.pOneBoxes[1] + this.pOneBoxes[2] + this.pOneBoxes[3] + this.pOneBoxes[4] + this.pOneBoxes[5] + this.pOneBoxes[6] + this.pOneBoxes[7] + this.pOneBoxes[8] + this.pOneBoxes[9] === 4 && this.pTwoBoxes[0] + this.pTwoBoxes[1] + this.pTwoBoxes[2] + this.pTwoBoxes[3] + this.pTwoBoxes[4] + this.pTwoBoxes[5] + this.pTwoBoxes[6] + this.pTwoBoxes[7] + this.pTwoBoxes[8] + this.pTwoBoxes[9] === 5 || this.pOneBoxes[0] + this.pOneBoxes[1] + this.pOneBoxes[2] + this.pOneBoxes[3] + this.pOneBoxes[4] + this.pOneBoxes[5] + this.pOneBoxes[6] + this.pOneBoxes[7] + this.pOneBoxes[8] + this.pOneBoxes[9] === 5 && this.pTwoBoxes[0] + this.pTwoBoxes[1] + this.pTwoBoxes[2] + this.pTwoBoxes[3] + this.pTwoBoxes[4] + this.pTwoBoxes[5] + this.pTwoBoxes[6] + this.pTwoBoxes[7] + this.pTwoBoxes[8] + this.pTwoBoxes[9] === 4) {
-    $("#winner").append("It's a DRAW!")
+  else if (this.pOneBoxes[0] + this.pOneBoxes[1] + this.pOneBoxes[2] + this.pOneBoxes[3] + this.pOneBoxes[4] + this.pOneBoxes[5] + this.pOneBoxes[6] + this.pOneBoxes[7] + this.pOneBoxes[8] === 4 && this.pTwoBoxes[0] + this.pTwoBoxes[1] + this.pTwoBoxes[2] + this.pTwoBoxes[3] + this.pTwoBoxes[4] + this.pTwoBoxes[5] + this.pTwoBoxes[6] + this.pTwoBoxes[7] + this.pTwoBoxes[8] === 5 || this.pOneBoxes[0] + this.pOneBoxes[1] + this.pOneBoxes[2] + this.pOneBoxes[3] + this.pOneBoxes[4] + this.pOneBoxes[5] + this.pOneBoxes[6] + this.pOneBoxes[7] + this.pOneBoxes[8] === 5 && this.pTwoBoxes[0] + this.pTwoBoxes[1] + this.pTwoBoxes[2] + this.pTwoBoxes[3] + this.pTwoBoxes[4] + this.pTwoBoxes[5] + this.pTwoBoxes[6] + this.pTwoBoxes[7] + this.pTwoBoxes[8] === 4) {
+    $("#winner").append("It's a DRAW!");
+    this.Winner = "yes";
+    $("#winner").fadeOut(3000);
   }
 
 }
@@ -43,17 +53,17 @@ Game.prototype.checkForWin = function () {
 $(document).ready(function () {
 
   var game = new Game();
-  var xPlayer = "";
-  var oPlayer = "";
-  // var wins = new winConditions();
+  var players = new Players();
+
   $("#startGame").click(function (event) {
     event.preventDefault();
 
-    xPlayer = $("input#xplayer-name").val(), ("X");
-    oPlayer = $("input#oplayer-name").val(), ("O");
-
+    players.pOneName = $("input#xplayer-name").val();
+    players.pTwoName = $("input#oplayer-name").val();
     $(".game").fadeIn();
     $("#start-screen").hide();
+    $("#xname").text(players.pOneName);
+    $("#oname").text(players.pTwoName);
 
 
   })
@@ -71,16 +81,48 @@ $(document).ready(function () {
 
         game.addBox();
         game.checkForWin();
-        console.log(game.pOneBoxes);
+        //       console.log(game.Winner);
+        if (game.Winner === "yes") {
+          game.pOneBoxes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+          game.pTwoBoxes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+          game.player1 = [];
+          game.player2 = [];
+          players.pTwoWins += 1;
+          $(".box").empty();
+          $("#winner").fadeOut(3000);
+          game.Winner = "";
+          game.turn = 1;
+          $("#twoWinCount").html(players.pTwoWins).val();
+
+        }
+        //       console.log(players.pOneWins)
+        //       console.log(game.pOneBoxes);
       }
       else if ((game.turn % 2) !== 0) {
         $(this).html("X");
         $("#playerTurn").html("O");
         game.turn += 1;
         game.player1.push(this.id);
-        console.log(game.player1);
+        //        console.log(game.player1);
         game.addBox();
         game.checkForWin();
+        if (game.Winner === "yes") {
+          game.pOneBoxes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+          game.pTwoBoxes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+          game.player1 = [];
+          game.player2 = [];
+          players.pOneWins += 1;
+          $(".box").empty();
+          $("#winner").fadeOut(3000);
+          console.log(players.pOneWins);
+          game.Winner = "";
+          game.turn = 1;
+          $("#oneWinCount").html(players.pOneWins).val();
+          if (game.turn == 1) {
+            $("#playerTurn").html("X");
+          }
+
+        }
         //console.log(game.player1);
         //      game.player1.sort();
         //    wins.checkForWinner();
